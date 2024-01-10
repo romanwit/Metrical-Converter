5eccc5c386b00fa7a0c6a7f598ffb93b28cc9f0d
romanwit <romanforresume@gmail.com>
2024-01-10 19:31:28 +0200

    Boostrap CSS imported to implement  their styles.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const ReactDOM = require("react-dom/client");
 const React = require("react");
 const Conversions = require("./components/conversions.jsx");
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(
     document.getElementById("app")
